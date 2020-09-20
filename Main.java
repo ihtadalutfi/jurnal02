820bf6f49935f5d2a57c50f6f2228699481d6cf8
ihtadalutfi <71589785+ihtadalutfi@users.noreply.github.com>
2020-09-20 05:57:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Ihtada Lutfi Ramadani");
+        System.out.println("Nim: 6706190061");
+        System.out.println("Kelas: 43-04");
+    }
+}
